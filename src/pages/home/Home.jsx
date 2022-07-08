045b5f763ad06b380a29eb118cc45445a63063c0
Viviana Duarte <yvduartef@gmail.com>
2022-07-08 08:55:42 -0500

    checkin search and image grid

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import GridImages from '../../components/gridImages/GridImages'
+import Header from '../../components/header/Header'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="main-container">
+      
+      </div>
+      <GridImages />
+    </div>
+  )
+}
+
+export default Home
